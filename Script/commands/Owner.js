b9643f1618f75ffc87f2d258d3fda360857742d1
ariyanbot2026-bit <ariyanbot2026@gmail.com>
2025-12-27 02:54:13 +0600

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -3,10 +3,10 @@ const fs = require("fs-extra");
 
 module.exports.config = {
   name: "owner",
-  version: "1.0.1",
+  version: "1.0.2",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
-  description: "Show Owner Info with styled box & random photo",
+  credits: "ARIYAN",
+  description: "Villain Owner Info with styled box & random photo",
   commandCategory: "Information",
   usages: "owner",
   cooldowns: 2
@@ -14,51 +14,48 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event }) {
 
-  
   const info = `
-╔═════════════════════ ✿
-║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
-╠═════════════════════ ✿
-║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗛𝗔𝗛𝗔𝗗𝗔𝗧 𝗦𝗔𝗛𝗨
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
-║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
-║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
-║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
-║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
-╠═════════════════════ ✿
-║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
-╠═════════════════════ ✿
-║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
-║ fb.com/100001039692046
-║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
-║ m.me/100001039692046
-║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
-║ wa.me/01882333052
-║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 :
-║ t.me/yoursahu
-╚═════════════════════ ✿
+╔═════════════════════ 🩸
+║ 😈 𝗩𝗜𝗟𝗟𝗔𝗜𝗡 𝗢𝗪𝗡𝗘𝗥 😈
+╠═════════════════════ 🩸
+║ 👑 𝗡𝗮𝗺𝗲 : 𝗔𝗥𝗜𝗬𝗔𝗡
+║ 🕶️ 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗶𝗹𝗲𝗻𝘁 𝗩𝗶𝗹𝗹𝗮𝗶𝗻
+║ 🎂 𝗔𝗴𝗲 : 18+
+║ 💔 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
+║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : שђ๏ภ๔ค๓เ
+║ 🌑 𝗔𝘁𝘁𝗶𝘁𝘂𝗱𝗲 : Calm but Dangerous
+║ 🧠 𝗠𝗶𝗻𝗱 : Cold & Focused
+╠═════════════════════ 🩸
+║ 🔥 𝗩𝗜𝗟𝗟𝗔𝗜𝗡 𝗥𝗨𝗟𝗘𝗦
+╠═════════════════════ 🩸
+║ • কথা কম, কাজ বেশি
+║ • বিশ্বাস করি না, প্রমাণ দেখি
+║ • শত্রু ভুলিনা, সময় আসলে শেষ করি
+╠═════════════════════ 🩸
+║ 🌐 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
+║ m.me/ariyan.ahmed.nai.Bangladesh.a
+╚═════════════════════ 🩸
 `;
 
   const images = [
-    "https://i.imgur.com/8WBso8x.png",
-    "https://i.imgur.com/0VZu5eY.png",
-    "https://i.imgur.com/bkixgPK.jpeg",
-    "https://i.imgur.com/z6G6L4c.jpeg"
+    "https://i.imgur.com/pyUAVFi.jpeg",
+    "https://i.imgur.com/Zp9nVDE.jpeg",
+    "https://i.imgur.com/yVHCfdZ.jpeg"
   ];
 
   const randomImg = images[Math.floor(Math.random() * images.length)];
+  const imgPath = __dirname + "/cache/owner.jpg";
 
   const callback = () => api.sendMessage(
     {
       body: info,
-      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+      attachment: fs.createReadStream(imgPath)
     },
     event.threadID,
-    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
+    () => fs.unlinkSync(imgPath)
   );
 
   return request(encodeURI(randomImg))
-    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
+    .pipe(fs.createWriteStream(imgPath))
     .on("close", () => callback());
 };
